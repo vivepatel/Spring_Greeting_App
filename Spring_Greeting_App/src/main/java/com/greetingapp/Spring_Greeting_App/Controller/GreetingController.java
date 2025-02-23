95f4044e9e43b6c275321d9b1f32cc0a558d507e
Vivekpatel <vp20032020@gmail.com>
2025-02-23 23:57:50 +0530

    [Vivek] Added UC1

---
+++ Spring_Greeting_App/src/main/java/com/greetingapp/Spring_Greeting_App/Controller/GreetingController.java
@@ -0,0 +1,43 @@
+package com.greetingapp.Spring_Greeting_App.Controller;
+
+
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+
+    @GetMapping
+    public Map<String, String> getGreeting() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Hello from GET method!");
+        return response;
+    }
+
+    @PostMapping
+    public Map<String, String> postGreeting(@RequestBody Map<String, String> request) {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Hello from POST method!");
+        response.put("data", request.getOrDefault("data", "No data provided"));
+        return response;
+    }
+
+    @PutMapping
+    public Map<String, String> putGreeting(@RequestBody Map<String, String> request) {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Hello from PUT method!");
+        response.put("updatedData", request.getOrDefault("data", "No data provided"));
+        return response;
+    }
+
+    @DeleteMapping
+    public Map<String, String> deleteGreeting() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Hello from DELETE method!");
+        return response;
+    }
+}
---
+++ Spring_Greeting_App/src/test/java/com/greetingapp/Spring_Greeting_App/SpringGreetingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greetingapp.Spring_Greeting_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGreetingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
