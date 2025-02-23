fbed61d0dc3263e0171c4aea2178272e2d422fdc
Vivekpatel <vp20032020@gmail.com>
2025-02-24 00:38:04 +0530

    [Vivek] Added UC3

--- Spring_Greeting_App/src/main/java/com/greetingapp/Spring_Greeting_App/Controller/GreetingController.java
+++ Spring_Greeting_App/src/main/java/com/greetingapp/Spring_Greeting_App/Controller/GreetingController.java
@@ -1,5 +1,4 @@
 package com.greetingapp.Spring_Greeting_App.Controller;
-
 import com.greetingapp.Spring_Greeting_App.Controller.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -19,37 +18,14 @@ public class GreetingController {
     }
 
     @GetMapping
-    public Map<String, String> getGreeting() {
-        Map<String, String> response = new HashMap<>();
-        response.put("message", "Hello from GET method!");
-        return response;
-    }
+    public Map<String, String> getGreeting(
+            @RequestParam(required = false) String firstName,
+            @RequestParam(required = false) String lastName) {
 
-    @GetMapping("/hello")
-    public String getGreetings() {
-        return greetingService.getGreeting();
-    }
+        String greetingMessage = greetingService.getGreeting(firstName, lastName);
 
-    @PostMapping
-    public Map<String, String> postGreeting(@RequestBody Map<String, String> request) {
         Map<String, String> response = new HashMap<>();
-        response.put("message", "Hello from POST method!");
-        response.put("data", request.getOrDefault("data", "No data provided"));
-        return response;
-    }
-
-    @PutMapping
-    public Map<String, String> putGreeting(@RequestBody Map<String, String> request) {
-        Map<String, String> response = new HashMap<>();
-        response.put("message", "Hello from PUT method!");
-        response.put("updatedData", request.getOrDefault("data", "No data provided"));
-        return response;
-    }
-
-    @DeleteMapping
-    public Map<String, String> deleteGreeting() {
-        Map<String, String> response = new HashMap<>();
-        response.put("message", "Hello from DELETE method!");
+        response.put("message", greetingMessage);
         return response;
     }
 }
--- Spring_Greeting_App/src/main/java/com/greetingapp/Spring_Greeting_App/Controller/GreetingService.java
+++ Spring_Greeting_App/src/main/java/com/greetingapp/Spring_Greeting_App/Controller/GreetingService.java
@@ -4,7 +4,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class GreetingService {
-    public String getGreeting() {
-        return "Hello World";
+    public String getGreeting(String firstName, String lastName) {
+        if (firstName != null && lastName != null) {
+            return "Hello " + firstName + " " + lastName + "!";
+        } else if (firstName != null) {
+            return "Hello " + firstName + "!";
+        } else if (lastName != null) {
+            return "Hello " + lastName + "!";
+        } else {
+            return "Hello World!";
+        }
     }
 }
